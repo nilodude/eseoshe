87b5df1c04fc32a0cc524619a8aa80527bf09072
nilodude <alphone@protonmail.com>
2022-10-12 11:21:53 +0000

    p.o.c.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,12 +14,17 @@ export class AppComponent implements OnInit {
   }
   ngOnInit(){
     this.randis = [];
-    this.randis.push(Math.floor(1+Math.random() * 9));
-    this.randis.push(Math.floor(1+Math.random() * 9));
-    this.randis.push(Math.floor(1+Math.random() * 9));
-    this.randis.push(Math.floor(1+Math.random() * 9));
-    this.randis.push(Math.floor(1+Math.random() * 9));
 
+    this.randis.push(this.random(1,3));
+    this.randis.push(this.random(2,2));
+    this.randis.push(this.random(1,2));
+    this.randis.push(this.random(2,2));
+
+    this.randis.push(12-(this.randis[0]+this.randis[1]+this.randis[2]+this.randis[3]));
+  }
+
+  random(min:number, max: number){
+    return Math.floor(min+(Math.random() * max));
   }
 
   }
