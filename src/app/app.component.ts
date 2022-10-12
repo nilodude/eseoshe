b0c22256be92d5e535fdc6e343b0670b39f81488
nilodude <alphone@protonmail.com>
2022-10-12 09:55:18 +0000

    prueva de konsekto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'eseoshe';
-}
+  randis: number[] = [];
+
+  constructor(){
+    
+  }
+  ngOnInit(){
+    this.randis = [];
+    this.randis.push(Math.floor(1+Math.random() * 9));
+    this.randis.push(Math.floor(1+Math.random() * 9));
+    this.randis.push(Math.floor(1+Math.random() * 9));
+    this.randis.push(Math.floor(1+Math.random() * 9));
+    this.randis.push(Math.floor(1+Math.random() * 9));
+
+  }
+
+  }
