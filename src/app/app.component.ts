dd3c07515de5800a5abe80c5a9605fa4988264c2
nilodude <alphone@protonmail.com>
2022-10-16 15:01:03 +0200

    need to apply color to each boxID

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,20 +12,15 @@ export class AppComponent implements OnInit {
   _1x1 = new Box();
   _2x1 = new Box();
   _1x2 = new Box();
-  boxes: any;
-  boxxes: Box[] = [];
+  boxesType: any;
+  boxPool: Box[] = [];
+  boxes: Box[] = [];
   pos: any;
   idCount: number = 0;
 
   constructor(){
     this.pos = [];
-    // for(let y =0; y< 10 ;y++){
-    //   let row = [];
-    //   for(let x =0; x< 10 ;x++){
-    //     row.push(new Cell(x,y));
-    //   }
-    //   this.pos.push(row);
-    // }
+    
   }
   ngOnInit(){
     this._2x1.ylen = 1;
@@ -36,16 +31,18 @@ export class AppComponent implements OnInit {
 
     this._1x1.tag = '1x1';
 
-    this.boxes= {'1x1': this._1x1, '1x2': this._1x2, '2x1': this._2x1};
-    this.boxxes =  [this._1x1];
+    this.boxesType= {'1x1': this._1x1, '1x2': this._1x2, '2x1': this._2x1};
+    this.boxPool =  [this._1x1];
 
     for(let y =0; y< 10 ;y++){
       for(let x =0; x< 10 ;x++){
         if(this.itsFree(x,y)){
           this.placeBox(x,y);
+          //this.applyStyle(x,y);
         }
       }
     }
+    
   }
 
   itsFree(x:number, y:number){
@@ -55,46 +52,70 @@ export class AppComponent implements OnInit {
     
     return isit && (!exists || (exists && this.pos[y][x].free));
   }
-  placeBox(x:number, y:number){
-    let box = this.getRandomBox(x,y);
 
-    
-  }
-  getRandomBox(x0:number, y0:number){
-    console.log(''+x0+','+y0)
-    let box;
-
-    if(this.itsFree(x0+1,y0)){
-      this.boxxes.push(this.boxes['1x2']);
-    }else if(this.itsFree(x0,y0+1)){
-      this.boxxes.push(this.boxes['2x1']);
-    }
-    const rand = this.random(0,2);
-    box = this.boxxes[rand];
-    box.id = this.idCount++;
-
-    console.log('new box' + box.tag);
+  placeBox(x0:number, y0:number){
+    let box = this.getRandomBox(x0,y0);
+    box.boxID = this.idCount++;
+    this.boxes.push(box);
+    console.log('new ' + box.tag);
 
     for(let y =y0; y<= y0 + box.ylen ;y++){
       let row = this.pos[y] ? this.pos[y] : [];
       for(let x =x0; x<= x0 + box.xlen ;x++){
         let cell = new Cell(x,y);
-        cell.boxID = box.id;
+        cell.boxID = box.boxID;
         cell.free = false;
-        
+        cell.color = box.color;
         box.cells.push(cell);
         row[x]=cell;
-        console.log('new cell: x='+x+'y='+y);
+        console.log('new cell x:'+x+',y:'+y);
+
       }
       this.pos[y] = row;
     }
+    
+  }
 
+  getRandomBox(x0:number, y0:number){
+    this.boxPool =  [this._1x1];
+    console.log(''+x0+','+y0)
+    let box;
+
+    if(this.itsFree(x0+1,y0)){
+      this.boxPool.push(this.boxesType['1x2']);
+    }else if(this.itsFree(x0,y0+1)){
+      this.boxPool.push(this.boxesType['2x1']);
+    }
+    const rand = this.random(this.boxPool.length);
+    box = this.boxPool[rand];
+    box.color = this.getRandomColor();
     return box;
   }
 
+  getRandomColor(){
+    const r = this.random(255);
+    const g = this.random(255);
+    const b = this.random(255);
+    const sr = r/this.random(4);
+    const sg = g/this.random(4);
+    const sb = b/this.random(4);
 
-  random(min:number, max: number){
-    return Math.floor(min+(Math.random() * max));
+    return 'rgb('+sr+', '+sg+', '+sb+')';
+  }
+
+  random(max: number){
+    return Math.floor(Math.random() * max);
+  }
+
+  applyStyle(x:number, y:number){
+    const cell = document.getElementsByClassName('box'+this.pos[y][x].boxID);
+    
+    if(cell){
+      const box = this.boxes.find(b=>b.boxID === this.pos[y][x].boxID );
+      if(box){
+        cell//.style.backgroundColor = box.color;
+      }
+    }
   }
 
   }
--- src/app/models.ts
+++ src/app/models.ts
@@ -1,9 +1,10 @@
 export class Box{
-    id: number = 0;
+    boxID: number = 0;
     cells: Cell[] = [];
     xlen:number = 0;
     ylen: number = 0;
     tag: string = ''
+    color: string = '';
 }
 
 export class Cell {
@@ -11,7 +12,7 @@ export class Cell {
     y: number = 0;
     boxID: number = 0;
     free: boolean = true;
-
+    color: string = '';
     constructor(x: number, y:number){
         this.x = x;
         this.y = y;
