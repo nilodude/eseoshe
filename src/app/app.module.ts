3d45b96146604bd3583ab4aa5977843e67da4cd0
nilodude <alphone@protonmail.com>
2022-10-23 12:07:05 +0200

    merged

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import {ButtonModule} from 'primeng/button';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
     CrazygridComponent
   ],
   imports: [
