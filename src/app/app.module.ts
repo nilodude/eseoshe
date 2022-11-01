429f4c2c456f5328917cda3ab24d8cf739e306bb
nilodude <alphone@protonmail.com>
2022-11-01 19:00:20 +0100

    beautiful tooltip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {DropdownModule} from 'primeng/dropdown';
 import { FormsModule }   from '@angular/forms';
 import {DialogModule} from 'primeng/dialog';
 import { ImageComponent } from './image/image.component';
+import {TooltipModule} from 'primeng/tooltip';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { ImageComponent } from './image/image.component';
     DropdownModule,
     FormsModule,
     BrowserAnimationsModule,
-    DialogModule
+    DialogModule,
+    TooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
