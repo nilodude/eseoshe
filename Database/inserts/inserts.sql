bfbaa3be57f4a86b39da50d03e48a8fe6a128e14
Tamara <bezdnatm@gmail.com>
2022-11-06 16:53:00 +0100

    basic inserts

---
+++ Database/inserts/inserts.sql
@@ -0,0 +1,34 @@
+INSERT INTO public.images
+(id, title, keywords, id_collection, height, width, date_publish, download, file_name)
+VALUES(1, 'Brown, white and black pastel circular drawing, seamless pattern. Tile background', '[''background'', ''circular'', ''abstract'', ''tile'', ''design'', ''seamless'', ''side'', ''white'', ''color'', ''black'', ''pattern'', ''yellow'', ''colorful'', ''texture'', ''line'', ''modern'', ''art'', ''wallpaper'', ''bright'', ''illustration'', ''backdrop'', ''brown'', ''decoration'', ''graphic'', ''purple'', ''light'', ''stripe'', ''rainbow'', ''lines'', ''paint'', ''geometric'', ''decorative'', ''element'', ''colored'', ''pastel'', ''retro'', ''creative'', ''striped'', ''template'', ''banner'', ''shape'', ''vibrant'', ''style'', ''beautiful'', ''fashion'', ''fabric'', ''paper'', ''trendy'', ''simple'']', 1, 2629, 2623, '2022-11-01', 0, '13');
+INSERT INTO public.images
+(id, title, keywords, id_collection, height, width, date_publish, download, file_name)
+VALUES(2, 'Multicolored brush strokes. Violet, pink and brown colors. Seamless pattern', '[''poster'', ''stroke'', ''paint'', ''splash'', ''oil'', ''damaged'', ''element'', ''gouache'', ''drawn'', ''yellow'', ''purple neon'', ''spotted'', ''ornament'', ''distressed'', ''artistic'', ''acrylic'', ''drawing'', ''decoration'', ''multicolor'', ''surface'', ''color'', ''purple'', ''seamless'', ''seamless background'', ''seamless texture'', ''seamless pattern'', ''brush stroke pattern'', ''spray paint'', ''background'', ''pattern'', ''texture'', ''watercolor'', ''print'', ''abstract'', ''art'', ''design'', ''colorful'', ''brush'', ''bright'', ''dye'', ''illustration'', ''tie'', ''neon'', ''space'', ''ink'', ''wallpaper'', ''vivid'', ''textile'', ''style'', ''hippie'']', 2, 3000, 3000, '2022-11-01', 0, '11.jpg');
+INSERT INTO public.images
+(id, title, keywords, id_collection, height, width, date_publish, download, file_name)
+VALUES(3, 'Green, beige, grey and white colored abstract blured brush strokes. Seamless pattern', '[''abstract'', ''abstract background'', ''acrylic'', ''art'', ''artistic'', ''background'', ''blurred'', ''blurred background'', ''bright'', ''brush'', ''brush strokes'', ''clean'', ''damaged'', ''decoration'', ''decorative'', ''design'', ''distressed'', ''drawing'', ''flow'', ''fluid'', ''gouache'', ''graphic'', ''grunge'', ''grungy'', ''illustration'', ''image'', ''messy'', ''modern'', ''nature'', ''oil'', ''paint'', ''paper'', ''pattern'', ''poster'', ''print'', ''reflection'', ''river'', ''seamless background'', ''seamless pattern'', ''season'', ''splash'', ''sponge'', ''stain'', ''stroke'', ''structure'', ''texture'', ''textured'', ''wallpaper'', ''watercolor'', ''white'']', 2, 3000, 3000, '2022-11-01', 0, '2.jpg');
+INSERT INTO public.images
+(id, title, keywords, id_collection, height, width, date_publish, download, file_name)
+VALUES(4, 'Abstract red brush strokes, heart shapes. White background', '[''abstract'', ''art'', ''artistic'', ''backdrop'', ''background'', ''brushstroke'', ''colorful'', ''concept'', ''decor'', ''decoration'', ''decorative'', ''design'', ''drawing'', ''drawn'', ''element'', ''fabric'', ''fashion'', ''february'', ''geometric'', ''gradient'', ''graffiti'', ''graphic'', ''hand drawn'', ''heart'', ''heart pattern'', ''hearts background'', ''illustration'', ''love'', ''love background'', ''ornament'', ''ornate'', ''painting'', ''pattern'', ''print'', ''red'', ''repeat'', ''repetition'', ''romantic'', ''seamless'', ''seamless pattern'', ''shape'', ''sketch'', ''style'', ''symbol'', ''template'', ''textile'', ''texture'', ''valentine'', ''wallpaper'', ''white'']', 3, 3000, 3000, '2022-11-01', 0, '8.jpg');
+INSERT INTO public.images
+(id, title, keywords, id_collection, height, width, date_publish, download, file_name)
+VALUES(5, 'Horizontal lines with abstract brush strokes, different colors, isolated. Seamless texture', '[''paintbrush'', ''brushstroke'', ''wave'', ''creative'', ''symmetry'', ''craft'', ''palette'', ''stripe'', ''drawn'', ''element'', ''brush'', ''watercolor'', ''ink brush stroke'', ''curve'', ''mosaic'', ''shapes'', ''ornate'', ''paint'', ''fractal'', ''isolated on white'', ''horizontal lines'', ''brush strokes'', ''seamless background'', ''seamless pattern'', ''seamless texture'', ''pattern'', ''design'', ''texture'', ''background'', ''art'', ''seamless'', ''graphic'', ''abstract'', ''vector'', ''ornament'', ''illustration'', ''color'', ''decoration'', ''textile'', ''print'', ''geometric'', ''decorative'', ''decor'', ''wallpaper'', ''blue'', ''simple'', ''fabric'', ''style'', ''line'', ''hand''] ', 4, 3000, 3000, '2022-11-01', 0, '6.jpg');
+
+
+INSERT INTO public.collections
+(id, "name")
+VALUES(1, 'Backgrounds');
+INSERT INTO public.collections
+(id, "name")
+VALUES(2, 'Trees');
+INSERT INTO public.collections
+(id, "name")
+VALUES(3, 'Animals');
+INSERT INTO public.collections
+(id, "name")
+VALUES(4, 'Lines');
+INSERT INTO public.collections
+(id, "name")
+VALUES(5, 'Flowers');
+
+
