824508a69f1c2f19a6b20a7d44bb7433b7315b41
nilodude <alphone@protonmail.com>
2024-03-17 20:39:37 +0100

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api: 'http://95.18.247.212:3000/pestock',
+  api: 'http://188.78.144.87:3000/pestock',
   admin: true,
   keywordsUrl: 'https://api.everypixel.com/v1/keywords',
   clientID: 'o5kHal8kW1ePdA0N7ANvZv5V',
