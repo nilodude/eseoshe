5dd680a04df7ad1a4a512702b1345b13fe39728d
nilodude <alphone@protonmail.com>
2024-08-30 14:43:48 +0200

    Updates

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  api: 'http://188.78.144.87:3000/pestock',
-  admin: true,
-  keywordsUrl: 'https://api.everypixel.com/v1/keywords',
-  clientID: 'o5kHal8kW1ePdA0N7ANvZv5V',
-  clientKey: 'tzKAQ9sgsb2pQPzX9Yf9UAYeGCBSXJDD6DRO99HacxTm05fj'
-};
